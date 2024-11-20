af63425c8b4edbce0d38935546e61bf5c49473d0
zhaoweijie1213 <zhaoweijie1108@qq.com>
2024-11-20 23:40:16 +0800

    跳转购物车

--- AixDutyFreeCrawlerApp/Program.cs
+++ AixDutyFreeCrawlerApp/Program.cs
@@ -2,6 +2,8 @@ using AixDutyFreeCrawler.App.Services;
 using AixDutyFreeCrawler.App.Components;
 using QYQ.Base.Common.IOCExtensions;
 using AixDutyFreeCrawler.App.Models;
+using Quartz;
+using Quartz.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,12 @@ builder.Services.AddRazorComponents()
 builder.Services.AddMultipleService("^AixDutyFreeCrawler");
 builder.Services.AddHostedService<WorkerService>();
 builder.Services.Configure<List<AccountModel>>(builder.Configuration.GetSection("Accounts"));
+builder.Services.AddQuartz().AddQuartzServer(options =>
+{
+    // when shutting down we want jobs to complete gracefully
+    options.WaitForJobsToComplete = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- AixDutyFreeCrawlerApp/Services/SeleniumService.cs
+++ AixDutyFreeCrawlerApp/Services/SeleniumService.cs
@@ -4,6 +4,8 @@ using QYQ.Base.Common.IOCExtensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using AixDutyFreeCrawler.App.Models;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AixDutyFreeCrawler.App.Services
 {
@@ -16,41 +18,49 @@ namespace AixDutyFreeCrawler.App.Services
         private readonly List<IWebDriver> drivers = new();
 
         /// <summary>
+        /// 
+        /// </summary>
+        private readonly IWebDriver monitorDriver = new ChromeDriver();
+
+        /// <summary>
         /// 开始任务
         /// </summary>
-        public Task CreateInstancesAsync(AccountModel account)
+        public async Task CreateInstancesAsync(AccountModel account)
         {
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.kixdutyfree.jp/cn/login/");
             //获取标题
             var title = driver.Title;
             logger.LogInformation("Title:{title}", title);
-            //查询是否有年龄确认dialog对话框
-            try
-            {
-                var ageConfirmDialog = driver.FindElement(By.ClassName("modal-dialog"));
-                var ageModalHeader = ageConfirmDialog.FindElement(By.ClassName("modal-header"));
-                if(ageModalHeader.FindElement(By.TagName("span")).Text == "年龄认证")
-                {
-                    //年龄确认
-                    var ageButton = ageConfirmDialog.FindElement(By.Id("age_yes"));
-                    ageButton.Click();
-                }
-            }
-            catch (NoSuchElementException e)
-            {
-                logger.LogWarning("TaskStartAsync:{Message}", e.Message);
-            }
-            //同意网站cookie
-            try
-            {
-                var trustCookie = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
-                trustCookie.Click();
-            }
-            catch (NoSuchElementException e)
-            {
-                logger.LogWarning("TaskStartAsync:{Message}", e.Message);
-            }
+
+            await Confirm(driver);
+
+            ////查询是否有年龄确认dialog对话框
+            //try
+            //{
+            //    var ageConfirmDialog = driver.FindElement(By.ClassName("modal-dialog"));
+            //    var ageModalHeader = ageConfirmDialog.FindElement(By.ClassName("modal-header"));
+            //    if(ageModalHeader.FindElement(By.TagName("span")).Text == "年龄认证")
+            //    {
+            //        //年龄确认
+            //        var ageButton = ageConfirmDialog.FindElement(By.Id("age_yes"));
+            //        ageButton.Click();
+            //    }
+            //}
+            //catch (NoSuchElementException e)
+            //{
+            //    logger.LogWarning("TaskStartAsync:{Message}", e.Message);
+            //}
+            ////同意网站cookie
+            //try
+            //{
+            //    var trustCookie = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
+            //    trustCookie.Click();
+            //}
+            //catch (NoSuchElementException e)
+            //{
+            //    logger.LogWarning("TaskStartAsync:{Message}", e.Message);
+            //}
             //检测登录按钮
             try
             {
@@ -70,16 +80,56 @@ namespace AixDutyFreeCrawler.App.Services
             }
             //保存实例到集合
             drivers.Add(driver);
-            return Task.CompletedTask;
         }
 
+        ///// <summary>
+        ///// 商品页面库存监控(不登录)
+        ///// </summary>
+        ///// <param name="productAddress"></param>
+        ///// <returns></returns>
+        //public async Task<bool> MonitorPagesAsync(string productAddress)
+        //{
+        //    bool status = true;
+        //    monitorDriver.Navigate().GoToUrl(productAddress);
+        //    await Confirm(monitorDriver);
+        //    return await CheckInventoryAsync(monitorDriver);
+        //}
+
+
+
         /// <summary>
-        /// 页面监控
+        /// 下单
         /// </summary>
         /// <returns></returns>
-        public Task MonitorPages()
+        public async Task OrderAsync(IWebDriver driver,string productAddress)
         {
-
+            bool status = await CheckInventoryAsync(driver);
+            if (status)
+            {
+                int orderNum = 0;
+                try
+                {
+                    //可购买最大数量
+                   var availabilityText = driver.FindElement(By.ClassName("availability-text")).Text;
+                    orderNum = ParseMaxOrder(availabilityText);
+                }
+                catch (NoSuchElementException e)
+                {
+                    logger.LogWarning("OrderAsync:{Message}", e.Message);
+                }
+                if (orderNum <= 0)
+                {
+                    orderNum = 20;
+                }
+                //输入数量
+                var productQuantity = driver.FindElement(By.Name("product-quantity"));
+                productQuantity.SendKeys(orderNum.ToString());
+                //添加到购物车
+                var addToCart = driver.FindElement(By.ClassName("add-to-cart"));
+                addToCart.Click();
+                //去购物车结算
+                driver.Navigate().GoToUrl("https://www.kixdutyfree.jp/cn/cart/");
+            }
         }
 
         ///// <summary>
@@ -106,5 +156,103 @@ namespace AixDutyFreeCrawler.App.Services
         //    await Task.WhenAll(tasks);
         //}
 
+        /// <summary>
+        /// 信息确认
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <returns></returns>
+        public Task Confirm(IWebDriver driver)
+        {
+            //查询是否有年龄确认dialog对话框
+            try
+            {
+                var ageConfirmDialog = driver.FindElement(By.ClassName("modal-dialog"));
+                var ageModalHeader = ageConfirmDialog.FindElement(By.ClassName("modal-header"));
+                if (ageModalHeader.FindElement(By.TagName("span")).Text == "年龄认证")
+                {
+                    //年龄确认
+                    var ageButton = ageConfirmDialog.FindElement(By.Id("age_yes"));
+                    ageButton.Click();
+                }
+            }
+            catch (NoSuchElementException e)
+            {
+                logger.LogWarning("TaskStartAsync:{Message}", e.Message);
+            }
+            //同意网站cookie
+            try
+            {
+                var trustCookie = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
+                trustCookie.Click();
+            }
+            catch (NoSuchElementException e)
+            {
+                logger.LogWarning("TaskStartAsync:{Message}", e.Message);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 检测库存
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <returns></returns>
+        public Task<bool> CheckInventoryAsync(IWebDriver driver)
+        {
+            bool status = true;
+            string productName = "";
+            try
+            {
+                productName = monitorDriver.FindElement(By.ClassName("product-name")).Text;
+            }
+            catch (NoSuchElementException e)
+            {
+                logger.LogWarning("MonitorPagesAsync:{Message}", e.Message);
+            }
+
+            try
+            {
+                //查询是否有库存
+                var text = monitorDriver.FindElement(By.XPath("//div[contains(@class, 'title') and contains(@class, 'availability-header')]"));
+                if (text.Text == "暂无库存")
+                {
+                    status = false;
+                    logger.LogInformation("MonitorPagesAsync:{productName}{Text}", productName, text.Text);
+                }
+                else
+                {
+                    logger.LogInformation("MonitorPagesAsync:{productName}{Text},{des}", productName, text.Text, monitorDriver.FindElement(By.ClassName("availability-text")).Text);
+                }
+            }
+            catch (NoSuchElementException e)
+            {
+                logger.LogWarning("MonitorPagesAsync:{Message}", e.Message);
+            }
+            return Task.FromResult(status);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static int ParseMaxOrder(string input)
+        {
+            // 使用正则表达式提取文本中的数字
+            string pattern = @"\d+";
+            Match match = Regex.Match(input, pattern);
+
+            if (match.Success)
+            {
+                // 将提取到的数字转换为整数
+                return int.Parse(match.Value);
+            }
+            else
+            {
+                return -1;
+            }
+        }
     }
 }

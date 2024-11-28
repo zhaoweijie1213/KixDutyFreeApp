46896920f44dbd1d12ecc8ae020b6f3aa6c50a88
zhaoweijie1213 <zhaoweijie1108@qq.com>
2024-11-28 23:36:43 +0800

    修改产品信息

--- AixDutyFreeCrawlerApp/Models/ProductMonitorInfo.cs
+++ AixDutyFreeCrawlerApp/Models/ProductMonitorInfo.cs
@@ -2,10 +2,24 @@
 {
     public class ProductMonitorInfo
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public string Name { get; set; } =string.Empty;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public OrderSetup Setup {  get; set; }
+    }
+
+    public enum OrderSetup
+    {
 
     }
 }
